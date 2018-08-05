0006e3e9c85189ae0908c0359d2bba43d78d6cbe
Administrator <Administrator@1-PC.LCSMK6>
2018-08-05 20:54:05 +0800

    修改问候语

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("I am zhongjianxu");
+		System.out.println("Hello£¬ÌÚÑ¶Î¢ÔÆ£¡");
 	}
+	
 }
